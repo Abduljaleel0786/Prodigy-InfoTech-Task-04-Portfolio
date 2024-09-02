6618f3936bc52164887df1d13647d2bae699c134
Abduljaleel0786 <167600901+Abduljaleel0786@users.noreply.github.com>
2024-09-02 13:09:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Web Developer","Abdul Jaleel" ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
